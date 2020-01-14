492098e4b2f936c6b9d8d3b635bdd26ea97f8ea6
PageSeanA <pageseana@gmail.com>
2020-01-14 16:00:15 -0600

    exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,42 @@
+
+#1
+# greeting = "Hello {}!"
+# name_of_user = input("What is your name? ")
+# length_of_name = len(name_of_user)
+# if length_of_name > 0:
+#     print(greeting.format(name_of_user))
+
+#2
+ greeting = "Hello {}!"
+name_of_user = input("What is your name? ")
+length_of_name = len(name_of_user)
+if length_of_name > 0:
+    print(greeting.format(name_of_user))
+
+
+
+
+
+
+#
+greeting = "HELLO {}!"
+name_of_user = input("What is your name? ")
+length_of_name = len(name_of_user)
+if length_of_name > 0:
+    print(greeting.upper(name_of_user))
+
+# elif:
+
+#     name_of_subject = input("What is your favorite subject? ")
+#     print(greeting .format(name_of_user, name_of_subject))
+# else:
+#     print("OK, I'll ask again some other time.")
+
+# greeting = "Hello %s, it is very nice to meet you and your friend %s!"
+# name_of_user = input("What is your name? ")
+# length_of_name = len(name_of_user)
+# if length_of_name > 0:
+#     name_of_friend = input("What is your friend's name? ")
+#     print(greeting % (name_of_user, name_of_friend))
+# else:
+#     print("OK, I'll ask again some other time.")

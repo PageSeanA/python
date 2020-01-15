20cf8842ba07e39bb500b1d106f09fa3db4f837c
PageSeanA <pageseana@gmail.com>
2020-01-14 23:52:42 -0600

    python

--- exercise1.py
+++ exercise1.py
@@ -1,42 +1,56 @@
-
+#Middle
 #1
-# greeting = "Hello {}!"
-# name_of_user = input("What is your name? ")
-# length_of_name = len(name_of_user)
-# if length_of_name > 0:
-#     print(greeting.format(name_of_user))
+# print("Total bill amount?")
+# bill = float(input())
+# print("Level of service? Good, Fiar or Bad")
+# service = input()
+# # % tipped for good 0.20, fiar 0.15, bad 0.10
+# if service == "good":
+#     total = bill + float(bill) * float(0.20)
+# elif service == "fair":
+#     total = bill + float(bill) * float(0.15)
+# else:
+#     total = bill + float(bill) * float(0.10)
+# print(total)
 
 #2
- greeting = "Hello {}!"
-name_of_user = input("What is your name? ")
-length_of_name = len(name_of_user)
-if length_of_name > 0:
-    print(greeting.format(name_of_user))
 
-
-
-
-
-
-#
-greeting = "HELLO {}!"
-name_of_user = input("What is your name? ")
-length_of_name = len(name_of_user)
-if length_of_name > 0:
-    print(greeting.upper(name_of_user))
-
-# elif:
-
-#     name_of_subject = input("What is your favorite subject? ")
-#     print(greeting .format(name_of_user, name_of_subject))
+# print("Total bill amount?")
+# bill = float(input())
+# print("Level of service? Good, Fiar or Bad")
+# service = input()
+# # % tipped for good 0.20, fiar 0.15, bad 0.10
+# if service == "good":
+#     total = bill + float(bill) * float(0.20)
+# elif service == "fair":
+#     total = bill + float(bill) * float(0.15)
 # else:
-#     print("OK, I'll ask again some other time.")
+#     total = bill + float(bill) * float(0.10)
+# print(total)
+# print("Split how many ways?")
+# split = total / float(input())
+# print(split)
 
-# greeting = "Hello %s, it is very nice to meet you and your friend %s!"
-# name_of_user = input("What is your name? ")
-# length_of_name = len(name_of_user)
-# if length_of_name > 0:
-#     name_of_friend = input("What is your friend's name? ")
-#     print(greeting % (name_of_user, name_of_friend))
-# else:
-#     print("OK, I'll ask again some other time.")
+#3
+# print("You have 0 coins. Do you want another, yes or no?")
+# answer = input("")
+# count = 0
+# while answer == "yes":
+#     count += 1
+#     print("You have", count)
+#     answer = input("Do you want another? ")
+# print("Bye")
+
+
+#4
+
+
+
+#5
+
+# Takes input from user for variables
+# width = int(input("Enter height: "))
+# height = int(input("Enter width: "))
+
+
+#6

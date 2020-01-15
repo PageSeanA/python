5e2d962e2a32af4589cd4d84e8708269da95d4e7
PageSeanA <pageseana@gmail.com>
2020-01-15 08:44:17 -0600

    phthon

--- exercise1.py
+++ exercise1.py
@@ -64,11 +64,7 @@
 #     n-=1
 
 #6
-# for row in range(1,5):
-#     for col in range(1,8):
-#         if row==4 or row+col==5 or col-row==3:
-#             print("*"," end="")
-#         else:
-#         print(end=" ")
-#     print()
-
+for i in range (1,11):
+    for n in range (1,11):
+        result = 1 * n
+        print(str(i) + " " + "X  "+str(n) + " = " + str(result))
